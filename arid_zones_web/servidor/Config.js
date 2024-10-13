279b47e24df1aef11d2883f7a74e2f4f9295fc23
perezlidia <lidiaperez@uas.edu.mx>
2024-10-13 14:13:44 -0700

    Add files via upload

---
+++ arid_zones_web/servidor/Config.js
@@ -0,0 +1,26 @@
+class Config {
+  	static isLocal() { return true; }
+  	static isLogeoDummy() { return false; }
+  	static getHost() { return '0.0.0.0'; }
+  	static getPuerto() { return 1235; }
+  	static getServidorGeoserver() { return '127.0.0.1:8083'; }
+  	static getConfigPostgres() { 
+  		return {
+		    idleTimeoutMillis: 30000,
+		    max: 25, // max number of clients in the pool
+		    host     : '127.0.0.1',
+		    port     : '5432',
+		    user     : 'postgres', 
+		    password : 'postgres', 
+		    database : 'zonas_aridas'
+		}
+	}
+	static getPathProyect(){
+		return 'file:///D:/zonas_aridas/zonas_aridas_web/';
+	}
+	static getPathProyectPython(){
+		return 'D:/zonas_aridas/zonas-aridas/';
+	}
+}	
+
+module.exports = Config;
